69638b5883f4578adbe90c79fbfc234dff4a5ba9
Jorge Martínez Carranza <jhmc000@hotmail.com>
2017-09-19 15:56:16 -0500

    Clase Abstracta

--- PracticaAutos/Form1.cs
+++ PracticaAutos/Form1.cs
@@ -10,13 +10,21 @@ using System.Windows.Forms;
 
 namespace PracticaAutos
 {
+    //Clase VariableG
+    public class VariablesG
+    {
+        //Declaracion de variables globales np, nl y p todas con sus respecivos get y set
+        public static string np { get; set; }
+        public static string nl { get; set; }
+        public static string p { get; set; }
+    }
     public partial class Form1 : Form
     {
         public Form1()
         {
             InitializeComponent();
         }
-
+        //Evento click del boton btn_FabricarAuto
         private void btn_FabricarAuto_Click(object sender, EventArgs e)
         {
             //If de validacion de checkbox
@@ -26,7 +34,35 @@ namespace PracticaAutos
                 Convencionales c = new Convencionales();
                 //Llamado a metodo NPuertas de la clase Convencionales
                 c.NPuertas();
+                //Manipulacion del texto de label por medio de la variable global
+                lbl_RPuertas.Text = VariablesG.np;
+                //Llamado a metodo NLlantas de la clase Convencionales
+                c.NLlantas();
+                //Manipulacion del texto de label por medio de la variable global
+                lbl_RLlantas.Text = VariablesG.nl;
+                //Llamado a metodo Potencia de la clase Convencionales
+                c.Potencia();
+                //Manipulacion del texto de label por medio de la variable global
+                lbl_RPotencia.Text = VariablesG.p;
                 MessageBox.Show("XD");
+            }
+            //If de validacion de checkbox
+            else if (cbx_ALuj.Checked == true)
+            {
+                //Inicializacion de clase Convencionales
+                DeLujo l = new DeLujo();
+                //Llamado a metodo NPuertas de la clase Convencionales
+                l.DNPuertas();
+                //Manipulacion del texto de label por medio de la variable global
+                lbl_RPuertas.Text = VariablesG.np;
+                //Llamado a metodo NLlantas de la clase Convencionales
+                l.DNLlantas();
+                //Manipulacion del texto de label por medio de la variable global
+                lbl_RLlantas.Text = VariablesG.nl;
+                //Llamado a metodo Potencia de la clase Convencionales
+                l.DPotencia();
+                //Manipulacion del texto de label por medio de la variable global
+                lbl_RPotencia.Text = VariablesG.p;
             }
         }
     }
@@ -41,26 +77,52 @@ namespace PracticaAutos
     //Clase Convencionales, que hereda de GenericMethod
     class Convencionales : GenericMethod
     {
-        //Inicializacion de la forma 1
-        Form1 fm = new Form1();
         //Llamado de metodo NPuertas 
         public override void NPuertas()
         {
+
             //Declaracion de variable de texto
-            string puertas = "2 a 4 puertas";
-            //Manipulacion del texto de label, desde forma 1
-            fm.lbl_RPuertas.Text = puertas.ToString();
-            MessageBox.Show("Hola");
+            VariablesG.np = "2 a 4 puertas";
         }
         public override void NLlantas()
         {
-            string llantas = "Llantas sencillas";
-            fm.lbl_RLlantas.Text = llantas.ToString();
+            //Declaracion de variable de texto
+            VariablesG.nl = "Llantas sencillas";
         }
         public override void Potencia()
         {
-            string potencia = "De 90 a 110 caballos de fuerza";
-            fm.lbl_RPotencia.Text = potencia.ToString();
+            //Declaracion de variable de texto
+            VariablesG.p = "De 90 a 110 caballos de fuerza";
+        }
+    }
+    class DeLujo
+    {
+        public virtual void DNPuertas()
+        {
+            string np = "2 puertas";
+        }
+        public virtual void DNLlantas()
+        {
+            string nl = "Llantas reforzadas";
+        }
+        public virtual void DPotencia()
+        {
+            string p = "De 110 a 250 caballos de fuerza";
+        }
+    }
+    class Formula1 : DeLujo
+    {
+        public override void DNPuertas()
+        {
+            string puertas = "Sin puertas";
+        }
+        public override void DNLlantas()
+        {
+            string llantas = "Llantas de alto rendimiento";
+        }
+        public override void DPotencia()
+        {
+            string potencia = "De 600 a 750 caballos de fuerza";
         }
     }
 }
