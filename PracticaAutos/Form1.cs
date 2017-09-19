b75522c321aa1711451abedf3b2e1a1a1af2098d
Jorge Martínez Carranza <jhmc000@hotmail.com>
2017-09-19 16:13:36 -0500

    Clase normal, Clase Polimorfismo(incompleta)

--- PracticaAutos/Form1.cs
+++ PracticaAutos/Form1.cs
@@ -49,18 +49,36 @@ namespace PracticaAutos
             //If de validacion de checkbox
             else if (cbx_ALuj.Checked == true)
             {
-                //Inicializacion de clase Convencionales
+                //Inicializacion de clase DeLujo
                 DeLujo l = new DeLujo();
-                //Llamado a metodo NPuertas de la clase Convencionales
+                //Llamado a metodo DNPuertas de la clase Convencionales
                 l.DNPuertas();
                 //Manipulacion del texto de label por medio de la variable global
                 lbl_RPuertas.Text = VariablesG.np;
-                //Llamado a metodo NLlantas de la clase Convencionales
+                //Llamado a metodo DNLlantas de la clase Convencionales
                 l.DNLlantas();
                 //Manipulacion del texto de label por medio de la variable global
                 lbl_RLlantas.Text = VariablesG.nl;
-                //Llamado a metodo Potencia de la clase Convencionales
+                //Llamado a metodo DPotencia de la clase Convencionales
                 l.DPotencia();
+                //Manipulacion del texto de label por medio de la variable global
+                lbl_RPotencia.Text = VariablesG.p;
+            }
+            else if (cbx_AFor1.Checked == true)
+            {
+                //Inicializacion de clase Convencionales
+                DeLujo l = new DeLujo();
+                DeLujo f = (Formula1)l;
+                //Llamado a metodo DNPuertas de la clase Convencionales
+                f.DNPuertas();
+                //Manipulacion del texto de label por medio de la variable global
+                lbl_RPuertas.Text = VariablesG.np;
+                //Llamado a metodo DNLlantas de la clase Convencionales
+                f.DNLlantas();
+                //Manipulacion del texto de label por medio de la variable global
+                lbl_RLlantas.Text = VariablesG.nl;
+                //Llamado a metodo DPotencia de la clase Convencionales
+                f.DPotencia();
                 //Manipulacion del texto de label por medio de la variable global
                 lbl_RPotencia.Text = VariablesG.p;
             }
@@ -80,18 +98,17 @@ namespace PracticaAutos
         //Llamado de metodo NPuertas 
         public override void NPuertas()
         {
-
-            //Declaracion de variable de texto
+            //Asignacion de variable global de texto
             VariablesG.np = "2 a 4 puertas";
         }
         public override void NLlantas()
         {
-            //Declaracion de variable de texto
+            //Asignacion de variable global de texto
             VariablesG.nl = "Llantas sencillas";
         }
         public override void Potencia()
         {
-            //Declaracion de variable de texto
+            //Asignacion de variable global de texto
             VariablesG.p = "De 90 a 110 caballos de fuerza";
         }
     }
@@ -99,30 +116,36 @@ namespace PracticaAutos
     {
         public virtual void DNPuertas()
         {
-            string np = "2 puertas";
+            //Asignacion de variable global de texto
+            VariablesG.np = "2 puertas";
         }
         public virtual void DNLlantas()
         {
-            string nl = "Llantas reforzadas";
+            //Asignacion de variable global de texto
+            VariablesG.nl = "Llantas reforzadas";
         }
         public virtual void DPotencia()
         {
-            string p = "De 110 a 250 caballos de fuerza";
+            //Asignacion de variable global de texto
+            VariablesG.p = "De 110 a 350 caballos de fuerza";
         }
     }
     class Formula1 : DeLujo
     {
         public override void DNPuertas()
         {
-            string puertas = "Sin puertas";
+            //Asignacion de variable global de texto
+            VariablesG.np = "Sin puertas";
         }
         public override void DNLlantas()
         {
-            string llantas = "Llantas de alto rendimiento";
+            //Asignacion de variable global de texto
+            VariablesG.nl = "Llantas de alto rendimiento";
         }
         public override void DPotencia()
         {
-            string potencia = "De 600 a 750 caballos de fuerza";
+            //Asignacion de variable global de texto
+            VariablesG.p = "De 600 a 750 caballos de fuerza";
         }
     }
 }
