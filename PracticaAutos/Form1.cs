2041f6258a5d724378bbda64df735c3b1b54e7e2
Jorge Martínez Carranza <jhmc000@hotmail.com>
2017-09-20 06:12:21 -0500

    ejemplo de Polimorfismo codigo comentado

--- PracticaAutos/Form1.cs
+++ PracticaAutos/Form1.cs
@@ -27,61 +27,85 @@ namespace PracticaAutos
         //Evento click del boton btn_FabricarAuto
         private void btn_FabricarAuto_Click(object sender, EventArgs e)
         {
-            //If de validacion de checkbox
-            if (cbx_AConv.Checked == true)
+            //Condiciones para validar que solo se marque una casilla
+            if (cbx_AConv.Checked && cbx_ALuj.Checked && cbx_AFor1.Checked)
             {
-                //Inicializacion de clase Convencionales
-                Convencionales c = new Convencionales();
-                //Llamado a metodo NPuertas de la clase Convencionales
-                c.NPuertas();
-                //Manipulacion del texto de label por medio de la variable global
-                lbl_RPuertas.Text = VariablesG.np;
-                //Llamado a metodo NLlantas de la clase Convencionales
-                c.NLlantas();
-                //Manipulacion del texto de label por medio de la variable global
-                lbl_RLlantas.Text = VariablesG.nl;
-                //Llamado a metodo Potencia de la clase Convencionales
-                c.Potencia();
-                //Manipulacion del texto de label por medio de la variable global
-                lbl_RPotencia.Text = VariablesG.p;
-                MessageBox.Show("XD");
+                MessageBox.Show("Solo debe marcar una casilla");
             }
-            //If de validacion de checkbox
-            else if (cbx_ALuj.Checked == true)
+            else if (cbx_AConv.Checked && cbx_ALuj.Checked)
             {
-                //Inicializacion de clase DeLujo
-                DeLujo l = new DeLujo();
-                //Llamado a metodo DNPuertas de la clase Convencionales
-                l.DNPuertas();
-                //Manipulacion del texto de label por medio de la variable global
-                lbl_RPuertas.Text = VariablesG.np;
-                //Llamado a metodo DNLlantas de la clase Convencionales
-                l.DNLlantas();
-                //Manipulacion del texto de label por medio de la variable global
-                lbl_RLlantas.Text = VariablesG.nl;
-                //Llamado a metodo DPotencia de la clase Convencionales
-                l.DPotencia();
-                //Manipulacion del texto de label por medio de la variable global
-                lbl_RPotencia.Text = VariablesG.p;
+                MessageBox.Show("Solo debe marcar una casilla");
             }
-            else if (cbx_AFor1.Checked == true)
+            else if (cbx_ALuj.Checked && cbx_AFor1.Checked)
             {
-                //Inicializacion de clase Convencionales
-                DeLujo l = new DeLujo();
-                DeLujo f = (Formula1)l;
-                //Llamado a metodo DNPuertas de la clase Convencionales
-                f.DNPuertas();
-                //Manipulacion del texto de label por medio de la variable global
-                lbl_RPuertas.Text = VariablesG.np;
-                //Llamado a metodo DNLlantas de la clase Convencionales
-                f.DNLlantas();
-                //Manipulacion del texto de label por medio de la variable global
-                lbl_RLlantas.Text = VariablesG.nl;
-                //Llamado a metodo DPotencia de la clase Convencionales
-                f.DPotencia();
-                //Manipulacion del texto de label por medio de la variable global
-                lbl_RPotencia.Text = VariablesG.p;
+                MessageBox.Show("Solo debe marcar una casilla");
             }
+            else if (cbx_AConv.Checked && cbx_AFor1.Checked)
+            {
+                MessageBox.Show("Solo debe marcar una casilla");
+            }
+            else
+            {
+                //If de validacion de checkbox
+                if (cbx_AConv.Checked == true)
+                {
+                    //Inicializacion de clase Convencionales
+                    Convencionales c = new Convencionales();
+                    //Llamado a metodo NPuertas de la clase Convencionales
+                    c.NPuertas();
+                    //Manipulacion del texto de label por medio de la variable global
+                    lbl_RPuertas.Text = VariablesG.np;
+                    //Llamado a metodo NLlantas de la clase Convencionales
+                    c.NLlantas();
+                    //Manipulacion del texto de label por medio de la variable global
+                    lbl_RLlantas.Text = VariablesG.nl;
+                    //Llamado a metodo Potencia de la clase Convencionales
+                    c.Potencia();
+                    //Manipulacion del texto de label por medio de la variable global
+                    lbl_RPotencia.Text = VariablesG.p;
+                }
+                //If de validacion de checkbox
+                else if (cbx_ALuj.Checked == true)
+                {
+                    //Inicializacion de clase DeLujo
+                    DeLujo l = new DeLujo();
+                    //Llamado a metodo DNPuertas de la clase Convencionales
+                    l.DNPuertas();
+                    //Manipulacion del texto de label por medio de la variable global
+                    lbl_RPuertas.Text = VariablesG.np;
+                    //Llamado a metodo DNLlantas de la clase Convencionales
+                    l.DNLlantas();
+                    //Manipulacion del texto de label por medio de la variable global
+                    lbl_RLlantas.Text = VariablesG.nl;
+                    //Llamado a metodo DPotencia de la clase Convencionales
+                    l.DPotencia();
+                    //Manipulacion del texto de label por medio de la variable global
+                    lbl_RPotencia.Text = VariablesG.p;
+                }
+                else if (cbx_AFor1.Checked == true)
+                {
+                    //Inicializacion de clase Formula 1
+                    Formula1 l = new Formula1();
+                    //Llamado a los metodos de la clase Formula1 por medio de la clase DeLujo a partir de una variable nueva
+                    DeLujo f = (Formula1)l;
+                    //Llamado a metodo DNPuertas de la clase Convencionales
+                    f.DNPuertas();
+                    //Manipulacion del texto de label por medio de la variable global
+                    lbl_RPuertas.Text = VariablesG.np;
+                    //Llamado a metodo DNLlantas de la clase Convencionales
+                    f.DNLlantas();
+                    //Manipulacion del texto de label por medio de la variable global
+                    lbl_RLlantas.Text = VariablesG.nl;
+                    //Llamado a metodo DPotencia de la clase Convencionales
+                    f.DPotencia();
+                    //Manipulacion del texto de label por medio de la variable global
+                    lbl_RPotencia.Text = VariablesG.p;
+                }
+                else
+                {
+                    MessageBox.Show("Debe marcar una casilla");
+                }
+            }            
         }
     }
     //Clase abstracta GenericMethod
@@ -95,17 +119,19 @@ namespace PracticaAutos
     //Clase Convencionales, que hereda de GenericMethod
     class Convencionales : GenericMethod
     {
-        //Llamado de metodo NPuertas 
+        //Llamado de metodo derivado NPuertas 
         public override void NPuertas()
         {
             //Asignacion de variable global de texto
             VariablesG.np = "2 a 4 puertas";
         }
+        //Llamado de metodo derivado NLlantas
         public override void NLlantas()
         {
             //Asignacion de variable global de texto
             VariablesG.nl = "Llantas sencillas";
         }
+        //Llamado de metodo derivado Potencia
         public override void Potencia()
         {
             //Asignacion de variable global de texto
@@ -114,16 +140,19 @@ namespace PracticaAutos
     }
     class DeLujo
     {
+        //Creacion de metodo virtual DNPuertas
         public virtual void DNPuertas()
         {
             //Asignacion de variable global de texto
             VariablesG.np = "2 puertas";
         }
+        //Creacion de metodo virtual DNLlantas
         public virtual void DNLlantas()
         {
             //Asignacion de variable global de texto
             VariablesG.nl = "Llantas reforzadas";
         }
+        //Creacion de metodo virtual DPotencia
         public virtual void DPotencia()
         {
             //Asignacion de variable global de texto
@@ -132,16 +161,19 @@ namespace PracticaAutos
     }
     class Formula1 : DeLujo
     {
+        //Llamado de metodo derivado DNPuertas
         public override void DNPuertas()
         {
             //Asignacion de variable global de texto
             VariablesG.np = "Sin puertas";
         }
+        //Llamado de metodo derivado DNLlantas
         public override void DNLlantas()
         {
             //Asignacion de variable global de texto
             VariablesG.nl = "Llantas de alto rendimiento";
         }
+        //Llamado de metodo derivado DPotencia
         public override void DPotencia()
         {
             //Asignacion de variable global de texto
